edca45c5716553f687241d03f02db9fb22698239
najdaoui <zobairnajdaoui1@gmail.com>
2023-02-22 17:48:16 +0100

    no change

--- 001_creat_table_ASCII/index.php
+++ 001_creat_table_ASCII/index.php
@@ -1,47 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style1.css">
     <title>Tab ASCII</title>
-    
+
 </head>
+
 <body>
     <h1>Thi is ASCII Alpha Upper and lower case:</h1>
-    
-    <table >
-    <tr>
-        <th>
-            Alpha
-        </th>
-        <th>
-            ASCII
-        </th>
-        <th>
-            Alpha
-        </th>
-        <th>
-            ASCII
-        </th>
-    </tr>
-    <?php for($i=ord("A");$i<=ord('Z');$i++){   ?>
-    <tr>
-          <td><?php
-            echo chr($i) 
-            ?></td>  
-          <td><?php
-          echo $i ?></td>  
-          <td><?php
-          echo chr($i+32) ?></td>  
-          <td><?php
-          echo $i+32?></td>  
-          
-    </tr>
-    <?php } ?>
-    
+
+    <table>
+        <tr>
+            <th>
+                Alpha
+            </th>
+            <th>
+                ASCII
+            </th>
+            <th>
+                Alpha
+            </th>
+            <th>
+                ASCII
+            </th>
+        </tr>
+        <?php for($i=ord("A");$i<=ord('Z');$i++){   ?>
+        <tr>
+
+            <td><?php echo chr($i);?></td>
+            <td><?php echo $i ?></td>
+            <td><?php echo chr($i+32) ?></td>
+            <td><?php echo $i+32 ?></td>
+
+        </tr>
+        <?php } ?>
+
 
     </table>
 </body>
+
 </html>
